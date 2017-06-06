98876aeb22be79b1b434154d5238822a58896004
Maxime Denoun <maxime_denoun@hotmail.com>
2017-06-06 21:38:36 +0300

    fix Issue#0001 Webpack not compiling at first launch when using export in module and it is hard to find why and where.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,8 +5,8 @@ import {NgModule} from "@angular/core";
 import {DashboardComponent} from "./dashboard.component";
 import {DashboardRouterModule} from "./dashboard-router.module";
 import {DashboardService} from "./dashboard.service";
-import NavigationModule from "../navigation/navigation.module";
 import {CommonModule} from "@angular/common";
+import {NavigationModule} from "../navigation/navigation.module";
 
 @NgModule({
   imports: [
